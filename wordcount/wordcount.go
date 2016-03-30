9e9258840b39e8127c9d9ae96c2965df5b95bf5a
YeLeo <yeleo1992@163.com>
2016-03-30 17:33:11 +0800

    improve string parse but don't work

--- wordcount/wordcount.go
+++ wordcount/wordcount.go
@@ -2,25 +2,25 @@ package wordcount
 
 import (
 	"fmt"
-	"github.com/yeleo/world/sortable"
+	//"github.com/yeleo/world/sortable"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-var m map[string]int
+var m map[interface{}]interface{}
 
 func Do() {
-	m = make(map[string]int, 100000)
+	m = make(map[interface{}]interface{}, 100000)
 	err := filepath.Walk("C:/Users/M/Desktop/BigText", walkFn)
 	if err != nil {
 		fmt.Println(err)
 	}
 	data := make([]string, 0, 100000)
-	for _, item := range sortable.SortableMap(m) {
-		data = append(data, item.Key+"\t"+item.Value)
-	}
+	//for _, item := range sortable.SortableMap(m) {
+	//	data = append(data, item.Key+"\t"+item.Value)
+	//}
 	err = ioutil.WriteFile("C:/Users/M/Desktop/BigText/Result-Go.txt", []byte(strings.Join(data, "\n")), os.ModeAppend)
 	if err != nil {
 		fmt.Println(err)
@@ -40,8 +40,13 @@ func walkFn(path string, info os.FileInfo, err error) error {
 		return err
 	}
 	//文本处理可能存在顺序问题
-	for _, v := range strings.Split(strings.TrimSpace(string(data)), " ") {
-		m[v]++
+	text := strings.Split(strings.TrimSpace(string(data)), " ")
+	for _, v := range text {
+		if m[v] != nil {
+			m[v] = m[v].(int) + 1
+		} else {
+			m[v] = 1
+		}
 	}
 	return nil
 }
